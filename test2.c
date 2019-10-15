80a2abc0f949e8ef1093871f721d776a90b5445e
Eugenio Doñaque <eugeniodonaque@gmail.com>
2019-10-15 09:38:45 +0200

    test2 nuevo

--- test2.c
+++ test2.c
@@ -4,7 +4,6 @@
  */
 
 #include <stdio.h>
-
 #include "my_lib.h"
 
 //#define NODES 100
@@ -15,22 +14,24 @@ struct my_data {
     char name[60];
 };
 
+
 int main() {
 
     struct my_stack *s1, *s2, *fs1;
     struct my_data *data, *data1, *data2;
     int len1, len2;
     int size1, size2;
+    int occupied_bytes=0;
 
     s1 = my_stack_init(sizeof(struct my_data));
     if (!s1) {
         puts("Error in my_stack_init()");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     printf("\ns1 initialized, size of data: %lu\n", sizeof(struct my_data));
 
     for (int i = 0; i < NODES; i++) {
-        data = malloc(sizeof(struct my_data)); // We must allocate static memory
+        data = malloc(sizeof(struct my_data)); 
         data->val = i;
         sprintf(data->name, "Value %d", i);
         if (my_stack_push(s1, data)) {
@@ -43,7 +44,7 @@ int main() {
     len1 = my_stack_len(s1);
     if (NODES != len1) {
         puts("Error in my_stack_len()");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     printf("s1 length: %d\n", len1);
 
@@ -51,49 +52,50 @@ int main() {
     s2 = my_stack_init(sizeof(struct my_data));
     if (!s2) {
         puts("Error in my_stack_init()");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     printf("\ns2 initialized, size of data: %lu\n", sizeof(struct my_data));
 
     for (int i = 0; i < NODES * 3; i++) {
-        data = malloc(sizeof(struct my_data)); // We must allocate static memory
+        data = malloc(sizeof(struct my_data)); 
         data->val = i;
         sprintf(data->name, "Value %d", i);
         if (my_stack_push(s2, data)) {
             printf("Error in my_stack_push (%d)\n", i);
-            exit(1);
+            exit(EXIT_FAILURE);
         }
         printf("New node in s2: (%d, %s)\n", data->val, data->name);
     }
     len2 = my_stack_len(s2);
     printf("s2 length: %d\n\n", len2);
 
+
     // Write the larger stack first
     puts("Writting the larger stack first (s2) in the file \"/tmp/my_stack.data\".");
     if (my_stack_write(s2, "/tmp/my_stack.data") <= 0) {
         puts("Error in my_stack_write (s2)");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     puts("Writting the smaller stack (s1), it must truncate the file.");
     // Now the smaller stack, it must truncate the file
     if (my_stack_write(s1, "/tmp/my_stack.data") != len1) {
         puts("Error in my_stack_write (s1)");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     puts("\nReading the file...");
     fs1 = my_stack_read("/tmp/my_stack.data");
     if (!fs1) {
         puts("Error in my_stack_read (fs1)");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     if (my_stack_len(s1) != my_stack_len(fs1)) {
         puts("Stacks s1 (initial stack 1) and fs1 (retrieved from file) don't have the same length");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     puts("s1 and the one retrieved from file (fs1) have the same length.");
-    puts("Comparing the data...");
+    puts("Comparing the data with pop...");
 
     // Test we can free the data and compare stacks s1 and fs1
     while ((data1 = my_stack_pop(s1))) {
@@ -111,27 +113,36 @@ int main() {
         free(data1);
         free(data2);
     }
+
     printf("size of data from s1: %d\t", size1);
     printf("size of data from fs1: %d\n", size2);
+    len1 = my_stack_len(s1);
+    occupied_bytes = len1*(sizeof(struct my_stack_node)+size1) + sizeof (struct my_stack);
+    printf ("\nWhole stack space s1: %d bytes\n", occupied_bytes);
+    if (occupied_bytes != my_stack_purge(s1)) {
+        puts("Error purging s1");
+        exit(EXIT_FAILURE);
+    }  
+    printf("Purging s1: %d bytes released\n", occupied_bytes);
 
-    if (my_stack_len(s1) != my_stack_purge(s1)) {
-        puts("Error purging s1: my_stack_len(s1) != my_stack_purge(s1)");
-        exit(1);
+    len2 = my_stack_len(s2);
+    occupied_bytes = len2*(sizeof(struct my_stack_node)+size1) + sizeof (struct my_stack);
+    printf ("\nWhole stack space s2: %d bytes\n", occupied_bytes);
+    if (occupied_bytes != my_stack_purge(s2)) {
+        puts("Error purging s2");
+        exit(EXIT_FAILURE);
     }
-    puts("\nPurging s1");
+    printf("Purging s2: %d bytes released\n", occupied_bytes);
 
-    if (my_stack_len(s2) != my_stack_purge(s2)) {
-        puts("Error purging s2: my_stack_len(s2) != my_stack_purge(s2)");
-        exit(1);
+    len1 = my_stack_len(fs1);
+    occupied_bytes = len1*(sizeof(struct my_stack_node)+size2) + sizeof (struct my_stack);
+    printf ("\nWhole stack space fs1: %d bytes\n", occupied_bytes);
+    if (occupied_bytes != my_stack_purge(fs1)) {
+        puts("Error purging fs1");
+        exit(EXIT_FAILURE);
     }
-    puts("Purging s2");
-
-    if (my_stack_len(fs1) != my_stack_purge(fs1)) {
-        puts("Error purging fs1: my_stack_len(fs1) != my_stack_purge(fs1)");
-        exit(1);
-    }
-    puts("Purging fs1");
+    printf("Purging fs1: %d bytes released\n", occupied_bytes);
 
     puts("\nAll tests passed :-)");
-    return 0; // To avoid warning in -Wall
+    return EXIT_SUCCESS; // To avoid warning in -Wall
 }
