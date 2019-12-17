c360943fb034dcbfc1b436b44f9e02ec36097fd7
RubenLopBon <56294473+RubenLopBon@users.noreply.github.com>
2019-12-17 10:04:03 +0100

    Update reader.c

--- Aventura3/reader.c
+++ Aventura3/reader.c
@@ -17,21 +17,21 @@ int main(int argc, char *argv[]) {
    int length = my_stack_len(pila);
    pila = my_stack_read(argv[1]);
    
-   //Comprobar que la pila contenga 
-   if (length<=0){
-       printf("Stack with 0 elements\n");
-       return 0;
-   }
 
    //Comprobar si la pila contiene menos de 10 elementos 
    if (length<10){
        printf("Stack with less than 10 elements\n");
        return 0;
    }
+    
+   //Si la pila tine mas de 10 elementos avisar
+   if(length>10){
+   printf("La pila tiene más de 10 elementos, se procederá a leer los 10 primeros\n");
+   }
 
    printf("Stack length: %d\n", length);
    int suma = 0, min = INT_MAX, max = 0;
-   for (int i = 0; i < length || i < 10; i++) {
+   for (int i = 0; i < 10 || i < 10; i++) {
        int *pop = my_stack_pop(pila);
        printf("%d \n", *pop);
        suma += *pop;
